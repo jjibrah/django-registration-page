2b397e5afa09ec9f0523b6c34614dc54c8f39364
jjibrah <ibrabdi109@gmail.com>
2023-03-02 10:41:00 +0300

    adding product2

--- djangoRegistrationPage/views.py
+++ djangoRegistrationPage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Products
 
 
 def register(request):
@@ -23,11 +24,15 @@ def home(request):
 
 @login_required
 def add_product(request):
+    if request.method == "POST":
+        p_name = request.POST.get('jina')
+        p_quantity = request.POST.get('kiasi')
+        p_price = request.POST.get('bei')
+        product = Products(prod_name=p_name, prod_quantity=p_quantity,
+                           prod_price=p_price)
+        product.save()
+        messages.success(request, 'Product saved successfully!')
+        return redirect('add-product')
     return render(request, 'add-product.html')
-
-
-
-
-
 
 # password=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
